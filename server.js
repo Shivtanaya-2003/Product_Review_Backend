86b60759169f8f43eadd59e4dfa7ddd9fc147570
Tanaya <Schoudharytanaya005@gmail.com>
2026-01-30 16:33:47 +0530

    added server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const middlewares = jsonServer.defaults();
 server.use(cors());
 server.use(middlewares);
 server.use(router);
+server.use(jsonServer.bodyParser);
 
 server.listen(10000, () => {
   console.log("Server running on port 10000");
